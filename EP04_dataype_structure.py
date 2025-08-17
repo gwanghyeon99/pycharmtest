ce527641070544efed37bd7f0ffb065e88d066dd
KGH <kimgh0726@naver.com>
2025-08-17 16:54:48 +0900

    EP02 commit

---
+++ EP04_dataype_structure.py
@@ -0,0 +1,44 @@
+from EP03_forMoon import student_scores
+
+# 정수
+age = 20
+print(age)
+print(type(age))
+
+# 실수
+weight = 80.5
+print(weight)
+print(type(weight))
+
+# 문자열
+greeting = "Hello World!"
+print(greeting)
+print(type(greeting))
+
+# 불리언
+is_adult = True
+print(is_adult)
+print(type(is_adult))
+
+# ---------------
+# 파이썬의 자료구조
+
+# 리스트 list
+fruits = ["사과", "바나나", "딸기"]
+print(fruits)
+print(type(fruits))
+
+# 튜플 tuple 원주율/기념일
+location = ('37.123','136.957')
+print(location)
+print(type(location))
+
+# 딕셔너리 = ctrl+f(정보값으로 다른 정보값 찾기)
+student_scores={'철수':90,'영희':85,'민수':95}
+print(student_scores)
+print(type(student_scores))
+
+# 세트 (중복 되는 것을 빼옴)
+colors={'red','red','blue','green','blue','yellow','black'}
+print(colors)
+print(type(colors))
